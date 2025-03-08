2fa41f7933a044347bb1a9f34d8063c5555bf4d9
rythmn1111 <rythmnmagnani0@gmail.com>
2025-03-08 13:39:48 +0530

    init2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import CustomCursor from "@/components/cursor";
-import { useEffect, useState } from "react";
+// import CustomCursor from "@/components/cursor";
+// import { useEffect, useState } from "react";
 export default function App({ Component, pageProps }: AppProps) {
-  const [isBrowser, setIsBrowser] = useState(false);
+  // const [isBrowser, setIsBrowser] = useState(false);
 
-  useEffect(() => {
-    // Only render cursor component on client-side
-    setIsBrowser(true);
-  }, []);
   return<>
    <Component {...pageProps} />
    {/* {isBrowser && <CustomCursor />} */}
